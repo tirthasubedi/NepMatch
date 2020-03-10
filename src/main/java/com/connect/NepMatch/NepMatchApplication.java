027991969dcfd16d52ff7bbce9c98d108514d116
[tirthasubedi] <[subedi.tirtha@gmail.com]>
2020-03-09 21:16:11 -0400

    Added Spring initializr

---
+++ src/main/java/com/connect/NepMatch/NepMatchApplication.java
@@ -0,0 +1,13 @@
+package com.connect.NepMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepMatchApplication.class, args);
+	}
+
+}
